d44bd99f28a19c14f99d34e6c4a6479396a830af
Ryosuke-256 <aaa@aa.com>
2024-05-27 18:02:29 +0900

    0527

--- ui_test/src/script.js
+++ ui_test/src/script.js
@@ -3,6 +3,9 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import ThreeMeshUI from 'three-mesh-ui';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry }  from 'three/examples/jsm/geometries/TextGeometry.js'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -11,31 +14,103 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 //scene.background = new THREE.Color(0.2,0.2,0.2)
 
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+fontLoader.load(
+    "./fonts/helvetiker_regular.typeface.json",
+    (font)=>{
+        const textGeometry = new TextGeometry(
+            "Hello Three.js text",
+            {
+                font:font,
+                size:50,
+                depth:0.1,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        const textMaterial = new THREE.MeshStandardMaterial()
+        const text = new THREE.Mesh(textGeometry,textMaterial)
+        text.position.set(200,200,0)
+        scene.add(text)
+    }
+)
+
+const helvitica = fontLoader.load(
+    "./fonts/helvetiker_regular.typeface.json",
+)
+
+/**
+ * UI
+ */
+//container
+const container = new ThreeMeshUI.Block({
+    height:160,width:100,padding:10,
+    //fontFamily:helvitica,
+    //fontFamily: "./fonts/helvetiker_regular.typeface.json",
+    fontFamily: './assets/Roboto-msdf.json',
+    fontTexture: './assets/Roboto-msdf.png',
+    textAlign: 'center',
+    justifyContent: 'center'
+})
+//block
+const imageBlock = new ThreeMeshUI.Block({
+    height:80,width:80,offset:10
+})
+const textBlock = new ThreeMeshUI.Block({
+    height:80,width:80,margin:10,offset:10
+})
+container.add(imageBlock,textBlock)
+
+//text
+const text = new ThreeMeshUI.Text({
+    content:'The spiny bush viper is known for its extremely keeled dorsal scales.',
+    fontColor:new THREE.Color(0xffffff),
+    fontSize:10.0,
+    backgroundOpacity: 0.0,
+    offset:1
+})
+textBlock.add(text)
+
+//image
+const ui_imageloader = new THREE.TextureLoader()
+ui_imageloader.load("./assets/spiny_bush_viper.jpg",(texture)=>{
+    imageBlock.set({backgroundTexture:texture})
+})
+
+scene.add(container)
 
 /**
  * Geometry
  */
 //plane1
-const plane1_geometry = new THREE.PlaneGeometry(10,10,10,10)
+const plane1_geometry = new THREE.PlaneGeometry(1000,1000,10,10)
 const textureLoader = new THREE.TextureLoader()
 const normalMapTexture = textureLoader.load("./texture/seaworn_stone_tile/seaworn_stone_tiles_nor_dx_1k.jpg")
 const plane1_material =new THREE.MeshStandardMaterial({color:0xffffff,side: THREE.DoubleSide, roughness:0.0, metalness: 0.0,normalMap:normalMapTexture})
 const plane1_mesh=new THREE.Mesh(plane1_geometry,plane1_material)
 plane1_mesh.rotation.set(Math.PI/2,0,0)
-plane1_mesh.position.set(0,-1,0)
+plane1_mesh.position.set(0,-100,0)
 plane1_mesh.receiveShadow = true
 scene.add(plane1_mesh)
 
 //box1
-const box1_geometry=new THREE.SphereGeometry(0.75,50,50)
+const box1_geometry=new THREE.SphereGeometry(50,50,50)
 const box1_material =new THREE.MeshStandardMaterial({color:0xff0000, roughness:0.0, metalness: 0.0})
 const box1_mesh=new THREE.Mesh(box1_geometry,box1_material)
+box1_mesh.position.set(0,200,0)
 box1_mesh.castShadow = true
 scene.add(box1_mesh)
 
 
 //cursor
-const cursor1_geometry = new THREE.SphereGeometry(0.1,10,10)
+const cursor1_geometry = new THREE.SphereGeometry(5,10,10)
 const cursor1_material = new THREE.MeshBasicMaterial({color:0x000000})
 const cursor1_mesh = new THREE.Mesh(cursor1_geometry,cursor1_material)
 cursor1_mesh.position.set(0,0,0)
@@ -44,28 +119,6 @@ scene.add(cursor1_mesh)
 /**
  * models
  */
-//gltf loader
-var object_gltf = null
-const gltfLoader = new GLTFLoader()
-gltfLoader.load(
-    "./models/gltf/teapot.gltf",
-    (gltf) =>{
-        object_gltf = gltf.scene.children[0] //children[0]はいらないときもあるので要確認
-
-        const coe = 0.5
-        object_gltf.scale.set(coe,coe,coe)
-        object_gltf.position.set(-3,-1,0)
-        object_gltf.material = new THREE.MeshStandardMaterial({color:0xff0000,roughness:0.5,metalness:0.5})
-        object_gltf.castShadow = true
-        scene.add(object_gltf)
-        console.log(object_gltf)
-    },(xhr)=>{
-        console.log((xhr.loaded/xhr.total*100)+'% loaded')
-    },(error)=>{
-        console.log('An error happened',error)
-    }
-)
-
 //obj loader
 var object_obj = null
 const objLoader = new OBJLoader()
@@ -74,9 +127,9 @@ objLoader.load(
     (obj) =>{
         object_obj = obj.children[0] //children[0]はいらないときもあるので要確認
 
-        const coe = 0.75
+        const coe = 50
         object_obj.scale.set(coe,coe,coe)
-        object_obj.position.set(3,0,0)
+        object_obj.position.set(200,0,0)
         object_obj.material = new THREE.MeshStandardMaterial({color:0xff0000,roughness:0.5,metalness:0.5})
         object_obj.castShadow = true
         scene.add(object_obj)
@@ -109,7 +162,7 @@ directionalLight.position.set(1,1,1)
 scene.add(directionalLight)
 
 //点光源
-const pointlight1 = new THREE.PointLight(0xffffff,10,0,1)
+const pointlight1 = new THREE.PointLight(0xffffff,200,0,1)
 pointlight1.position.set(0,0,0)
 pointlight1.castShadow = true
 scene.add(pointlight1)
@@ -152,39 +205,16 @@ window.addEventListener("dblclick",() =>
 /**
  * Camera
  */
-var position_ratio = 100
 // Base camera
 const fov = 75
 const dist =(fov) =>{
     const fovRad= (fov/2)*(Math.PI/180)
-    const dist = ((sizes.height/position_ratio)/2)/Math.tan(fovRad)
+    const dist = (sizes.height/2)/Math.tan(fovRad)
     return dist
 }
-const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.01, dist(fov)*100)
+const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.1, dist(fov)*10)
 camera.position.set(0,0,dist(fov))
 scene.add(camera)
-
-//number key to camera
-document.addEventListener("keydown",(e)=>{
-    if(e.keyCode == 49) {
-        camera.position.set(0,0,dist(fov))
-    }
-    if(e.keyCode == 50) {
-        camera.position.set(dist(fov),0,0)
-    }
-    if(e.keyCode == 51) {
-        camera.position.set(0,0,-dist(fov))
-    }
-    if(e.keyCode == 52) {
-        camera.position.set(-dist(fov),0,0)
-    }
-    if(e.keyCode == 53) {
-        camera.position.set(0,dist(fov),0)
-    }
-    if(e.keyCode == 54) {
-        camera.position.set(0,-dist(fov),0)
-    }
-})
 
 /**
  * Renderer
@@ -203,7 +233,7 @@ renderer.shadowMap.enabled = true // 影
 
 //controls
 const controls = new OrbitControls( camera, canvas)
-//controls.enableDamping = true
+controls.enableDamping = true
 
 
 /**
@@ -217,16 +247,16 @@ const mouse_window_normal =new THREE.Vector2()
 window.addEventListener('mousemove',e =>
 {
     //WebGLマウス座標
-    mouse_webGL.x=(e.clientX-(sizes.width/2))/position_ratio
-    mouse_webGL.y=(-e.clientY+(sizes.height/2))/position_ratio
+    mouse_webGL.x=e.clientX-(sizes.width/2)
+    mouse_webGL.y=-e.clientY+(sizes.height/2)
 
     //WebGLマウス座標の正規化
-    mouse_webGL_normal.x=(mouse_webGL.x*2/sizes.width)/position_ratio
-    mouse_webGL_normal.y=(mouse_webGL.y*2/sizes.height)/position_ratio
+    mouse_webGL_normal.x=(mouse_webGL.x*2/sizes.width)
+    mouse_webGL_normal.y=(mouse_webGL.y*2/sizes.height)
 
     //Windowマウス座標の正規化
-    mouse_window_normal.x=(e.clientX/sizes.width)*2/position_ratio-1
-    mouse_window_normal.y=-(e.clientY/sizes.height)*2/position_ratio+1
+    mouse_window_normal.x=(e.clientX/sizes.width)*2-1
+    mouse_window_normal.y=-(e.clientY/sizes.height)*2+1
 
 //WebGL関連
     //ライトの座標
@@ -254,13 +284,15 @@ const animate = () =>
     //second
     const sec = performance.now()/1000
 
+    //geometry animation
     box1_mesh.rotation.y=sec*(Math.PI/4)
-    if(object_gltf!=null){
-        object_gltf.rotation.z=sec*(Math.PI/4)
-    }
     if(object_obj!=null){
         object_obj.rotation.y=sec*(Math.PI/4)
     }
+
+    //UI
+    ThreeMeshUI.update()
+
     // Call tick again on the next frame
     window.requestAnimationFrame(animate)
 }
